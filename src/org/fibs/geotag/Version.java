505422195836ee3795f20d34bc63515c15ecbfc3
Andreas Schneider <geotag@fibs.me.uk>
2016-07-03 13:57:13 +0000

    Version 0.098

--- src/org/fibs/geotag/Version.java
+++ src/org/fibs/geotag/Version.java
@@ -51,7 +51,7 @@ public final class Version {
   public static final int MAJOR = 0;
 
   /** The minor version number. */
-  public static final int MINOR = 97;
+  public static final int MINOR = 98;
 
   /** The build date of the program. */
   public static final String BUILD_DATE;
