ecef7c618fb260669f2ec3da146b4eb243da807f
Andreas Schneider <geotag@fibs.me.uk>
2017-01-02 13:01:54 +0000

    Version 0.102

--- src/org/fibs/geotag/util/FileUtilTest.java
+++ src/org/fibs/geotag/util/FileUtilTest.java
@@ -1,6 +1,6 @@
 /**
  * Geotag
- * Copyright (C) 2007-2016 Andreas Schneider
+ * Copyright (C) 2007-2017 Andreas Schneider
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
